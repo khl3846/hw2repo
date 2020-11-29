6bc99176d683ab4ddcbd1420fe352ce06dd4ff80
hrkim <1991330@hansung.ac.kr>
2020-11-29 19:03:32 +0900

    sum program

--- program.c
+++ program.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
-int fib(int n)
+int sum(int n)
 {
-  if (n == 0) return 0;
   if (n == 1) return 1;
-  return fib(n-1) + fib(n-2);
+  return n + sub(n-1);
 }
 
 int main()
 {
   printf("Hello world!\n");
-  printf("%d\n", fib(5));
+  printf("%d\n", sub(5));
   return 0;
 }
